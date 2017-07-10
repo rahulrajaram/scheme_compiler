fc398be45ec691f4a2153a77b97e2f558d06c2d0
Rahul Rajaram <rahulrajaram2005@gmail.com>
2017-07-09 17:21:38 -0700

    std::exit(1) when the source program file cannot be found

--- src/lexer/main.cpp
+++ src/lexer/main.cpp
@@ -17,9 +17,14 @@ int main () {
 	SpdlogLogger spdlog_console = spdlog::stdout_color_mt("console");
 	print_welcome_message (spdlog_console);
 
-	IfStream source_file("../../config/fib.sc");
+	IfStream source_file("../../config/assertion-violation.sc");
 	IfStream lang_spec_file("../../config/syntax.txt");
 	
+	if (!source_file.is_open()) {
+		std::cout << "File not present!\n";
+
+		std::exit(1);
+	}
 	auto lexer_input = Atrium::LexicalAnalysis::LexerInput(source_file, lang_spec_file);
 	auto lexer = Atrium::LexicalAnalysis::Lexer(lexer_input);
 
