7fa361b861eb072c4763e585479bafbe051bc42b
Rahul Rajaram <rahulrajaram2005@gmail.com>
2017-07-10 20:13:59 -0700

    Add additional test cases

--- atrium/lexer/test/tokenizer_tests.cpp
+++ atrium/lexer/test/tokenizer_tests.cpp
@@ -1,10 +1,14 @@
 #include <spdlog/spdlog.h>
 
+#include "../../global_aliases.h"
+
 #include "../lexer_input.h"
 #include "../lexer.h"
 #include "test_helper.h"
 
 Atrium::TestCase test_case;
+
+SpdlogLogger spdlog_console = spdlog::stdout_color_mt("console");
 
 void test_assertion_violation_scm__expected_and_actual_equal() {
 	std::vector <std::string> expected =
@@ -12,7 +16,88 @@ void test_assertion_violation_scm__expected_and_actual_equal() {
 
 	IfStream source_file("../../../config/assertion-violation.scm");
 
-	SpdlogLogger spdlog_console = spdlog::stdout_color_mt("console");
+	auto lexer = Atrium::LexicalAnalysis::Lexer(source_file);
+
+	auto actual_token_vector = lexer.parse(spdlog_console);
+
+	test_case.assert_vectors_equal(
+		expected,
+		actual_token_vector.token_vector
+	);
+}
+
+void test_boolean_scm() {
+	std::vector <std::string> expected =
+ 		test_case.load_expected_token_vector("../../../config/boolean.tok");
+
+	IfStream source_file("../../../config/boolean.scm");
+
+	auto lexer = Atrium::LexicalAnalysis::Lexer(source_file);
+
+	auto actual_token_vector = lexer.parse(spdlog_console);
+
+	test_case.assert_vectors_equal(
+		expected,
+		actual_token_vector.token_vector
+	);
+}
+
+void test_digits_scm() {
+	std::vector <std::string> expected =
+ 		test_case.load_expected_token_vector("../../../config/digits.tok");
+
+	IfStream source_file("../../../config/digits.scm");
+
+
+	auto lexer = Atrium::LexicalAnalysis::Lexer(source_file);
+
+	auto actual_token_vector = lexer.parse(spdlog_console);
+
+	test_case.assert_vectors_equal(
+		expected,
+		actual_token_vector.token_vector
+	);
+}
+
+void test_fib_scm() {
+	std::vector <std::string> expected =
+ 		test_case.load_expected_token_vector("../../../config/fib.tok");
+
+	IfStream source_file("../../../config/fib.scm");
+
+	auto lexer = Atrium::LexicalAnalysis::Lexer(source_file);
+
+	auto actual_token_vector = lexer.parse(spdlog_console);
+
+	test_case.assert_vectors_equal(
+		expected,
+		actual_token_vector.token_vector
+	);
+}
+
+void test_identifier_name_with_question_mark_scm() {
+	std::vector <std::string> expected =
+ 		test_case.load_expected_token_vector("../../../config/identifier_name_with_question_mark.tok");
+
+	IfStream source_file("../../../config/identifier_name_with_question_mark.scm");
+
+	auto lexer = Atrium::LexicalAnalysis::Lexer(source_file);
+
+	auto actual_token_vector = lexer.parse(spdlog_console);
+
+	test_case.assert_vectors_equal(
+		expected,
+		actual_token_vector.token_vector
+	);
+}
+
+
+void test_prog1_scm() {
+	std::vector <std::string> expected =
+ 		test_case.load_expected_token_vector("../../../config/prog1.tok");
+
+	IfStream source_file("../../../config/prog1.scm");
+
 
 	auto lexer = Atrium::LexicalAnalysis::Lexer(source_file);
 
@@ -43,6 +128,22 @@ int main () {
 	test_case.test_name = "test_assertion_violation_scm__expected_and_actual_equal";
 	test_case.source_file = "assertion-violation.scm";
 	test_case.run(test_assertion_violation_scm__expected_and_actual_equal);
+
+	test_case.test_name = "test_boolean_scm";
+	test_case.source_file = "boolean.scm";
+	test_case.run(test_boolean_scm);
+
+	test_case.test_name = "test_digits_scm";
+	test_case.source_file = "digits.scm";
+	test_case.run(test_digits_scm);
+
+	test_case.test_name = "test_fib_scm";
+	test_case.source_file = "fib.scm";
+	test_case.run(test_fib_scm);
+
+	test_case.test_name = "test_identifier_name_with_question_mark_scm";
+	test_case.source_file = "identifier_name_with_question_mark.scm";
+	test_case.run(test_identifier_name_with_question_mark_scm);
 
 	return 0;
 }
