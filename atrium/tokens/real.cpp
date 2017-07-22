700cf1852a79aa2a102d24030c9c36a4b6975534
Rahul Rajaram <rahulrajaram2005@gmail.com>
2017-07-22 09:25:05 -0700

    Make real.cpp code more compact

--- atrium/tokens/real.cpp
+++ atrium/tokens/real.cpp
@@ -3,49 +3,49 @@
 namespace Atrium {
 	bool Token::is_real_2() {
 		if (starts_with_prefix("+") || starts_with_prefix("-")) {
-			Token possibly_ureal_2 = Token(token.substr(1));
-
-			if (possibly_ureal_2.is_ureal_2()) {
+			if (Token(token.substr(1)).is_ureal_2()) {
 				return true;
 			}
+
+			return false;
 		}
 
-		return false;
+		return is_ureal_2();
 	}
 
 	bool Token::is_real_8() {
 		if (starts_with_prefix("+") || starts_with_prefix("-")) {
-			Token possibly_ureal_8 = Token(token.substr(1));
-
-			if (possibly_ureal_8.is_ureal_8()) {
+			if (Token(token.substr(1)).is_ureal_8()) {
 				return true;
 			}
+
+			return false;
 		}
 
-		return false;
+		return is_ureal_16();
 	}
 
 	bool Token::is_real_10() {
 		if (starts_with_prefix("+") || starts_with_prefix("-")) {
-			Token possibly_ureal_10 = Token(token.substr(1));
-
-			if (possibly_ureal_10.is_ureal_10()) {
+			if (Token(token.substr(1)).is_ureal_10()) {
 				return true;
 			}
+
+			return false;
 		}
 
-		return false;
+		return is_ureal_10();
 	}
 
 	bool Token::is_real_16() {
 		if (starts_with_prefix("+") || starts_with_prefix("-")) {
-			Token possibly_ureal_16 = Token(token.substr(1));
-
-			if (possibly_ureal_16.is_ureal_16()) {
+			if (Token(token.substr(1)).is_ureal_16()) {
 				return true;
 			}
+
+			return false;
 		}
 
-		return false;
+		return is_ureal_16();
 	}
 }
