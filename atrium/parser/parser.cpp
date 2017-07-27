52aaadf6fa32c09edd83eafea6f650ba246cd052
Rahul Rajaram <rahulrajaram2005@gmail.com>
2017-07-26 18:25:03 -0700

    Add support to transformer expression

--- atrium/parser/parser.cpp
+++ atrium/parser/parser.cpp
@@ -606,6 +606,160 @@ namespace Atrium {
 		return true;
 	}
 
+	bool Parser::is_pattern () {
+		std::size_t token_vector_index_at_entry = token_vector_index;
+
+		if (is_pattern_identifier ()) {
+			return true;
+		}
+
+		if (is_constant ()) {
+			return true;
+		}
+
+		if (current_token () == "(") {
+			token_vector_index ++;
+
+			if (is_template_element()) {
+				while (is_template_element());
+
+				if (current_token == ".") {
+					token_vector_index ++;
+
+					if (!is_template_element()) {
+						token_vector_index = token_vector_index_at_entry;
+						return false;
+					}
+
+					return true;
+				}
+
+				if (current_token () != ")") {
+						token_vector_index = token_vector_index_at_entry;
+						return false;
+				}
+				token_vector_index ++;
+			}
+
+			return true;
+		}
+
+		if (current_token() != "#") {
+			token_vector_index = token_vector_index_at_entry;
+			return false;
+		}
+		token_vector_index ++;
+
+		if (current_token() != "(") {
+			token_vector_index = token_vector_index_at_entry;
+			return false;
+		}
+		token_vector_index ++;
+
+		while(is_template_element());
+
+		if (current_token() != ")") {
+			token_vector_index = token_vector_index_at_entry;
+			return false;
+		}
+
+		return true;
+	}
+
+	bool Parser::is_template () {
+		std::size_t token_vector_index_at_entry = token_vector_index;
+
+		if (is_pattern_identifier ()) {
+			return true;
+		}
+
+		if (is_template_datum ()) {
+			return true;
+		}
+
+		if (current_token () != "(") {
+			token_vector_index ++;
+
+			if (is_template_element()) {
+				while (is_template_element());
+
+				if (current_token == ".") {
+					token_vector_index ++;
+
+					if (!is_template_element()) {
+						token_vector_index = token_vector_index_at_entry;
+						return false;
+					}
+				}
+			}
+	
+			if (current_token () != ")") {
+				token_vector_index = token_vector_index_at_entry;
+				return false;
+			}
+
+			token_vector_index ++;
+
+			return true;
+		}
+
+		if (current_token() != "#") {
+			token_vector_index = token_vector_index_at_entry;
+			return false;
+		}
+		token_vector_index ++;
+
+		if (current_token() != "(") {
+			token_vector_index = token_vector_index_at_entry;
+			return false;
+		}
+		token_vector_index ++;
+
+		while(is_template_element());
+
+		if (current_token() != ")") {
+			token_vector_index = token_vector_index_at_entry;
+			return false;
+		}
+
+		return true;
+	}
+
+	bool Parser::is_template_element () {
+		if (! is_template ()) {
+			token_vector_index = token_vector_index_at_entry;
+			return false;
+		}
+
+		is_ellipsis ();
+
+		return true;
+	}
+
+	bool Parser::is_template_datum () {
+		return is_constant();
+	}
+
+	bool Parser::is_pattern_identifier () {
+		if (current_token_type() == "IDENTIFIER") {
+			if (current_token() != "...") {
+				token_vector_index ++;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	bool Parser::is_ellipsis () {
+		if (current_token() == "...") {
+			token_vector_index ++;
+			return true;
+		}
+
+		return false;
+	}
+
 	bool Parser::parse () {
 		while (token_vector_index < token_vector.size()) {
 			if (! is_form ()) {
