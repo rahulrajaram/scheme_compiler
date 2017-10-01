f066a878cee3e5618b198f27ecf6256624429dca
Rahul Rajaram <rahulrajaram2005@gmail.com>
2017-09-30 17:36:56 -0700

    Allow expressions as datums

--- atrium/parser/datum.cpp
+++ atrium/parser/datum.cpp
@@ -23,8 +23,16 @@ namespace Atrium {
 			return true;
 		}
 
-		while (is_vector ()) {
+		if (is_vector ()) {
 			print_production(token_vector_index, "vector");
+			return true;
+		};
+
+		// syntax.txt does not list this production rule, but I
+		// this is meaningful. Consider:
+		//
+		// 		'("yolo" (if (> 1 2) #t #f))
+		if (is_expression ()) {
 			return true;
 		};
 
