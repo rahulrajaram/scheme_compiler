24ec33378872e092885710ff9b65a9b81a620a44
Rahul Rajaram <rahulrajaram2005@gmail.com>
2017-09-30 17:36:14 -0700

    Fix token_vector_index resetting bug

--- atrium/parser/case_clause.cpp
+++ atrium/parser/case_clause.cpp
@@ -6,28 +6,29 @@ namespace Atrium {
 		std::size_t token_vector_index_at_entry = token_vector_index;
 
 		if (!is_left_paren()) {
+			token_vector_index = token_vector_index_at_entry;
 			return false;
 		}
 	
 		if (!is_left_paren()) {
-			token_vector_index_at_entry = token_vector_index;
+			token_vector_index = token_vector_index_at_entry;
 			return false;
 		}
 
 		while(is_datum());
 
 		if (!is_right_paren()) {
-			token_vector_index_at_entry = token_vector_index;
+			token_vector_index = token_vector_index_at_entry;
 			return false;
 		}
 
 		if (!is_sequence()) {
-			token_vector_index_at_entry = token_vector_index;
+			token_vector_index = token_vector_index_at_entry;
 			return false;
 		}	
 
 		if (!is_right_paren()) {
-			token_vector_index_at_entry = token_vector_index;
+			token_vector_index = token_vector_index_at_entry;
 			return false;
 		}		
 
