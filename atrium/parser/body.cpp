59e0e710151dd9648988fff61af8c83c7e77494d
Rahul Rajaram <rahulrajaram2005@gmail.com>
2017-09-30 17:35:48 -0700

    Allow empty bodies

--- atrium/parser/body.cpp
+++ atrium/parser/body.cpp
@@ -5,13 +5,10 @@ namespace Atrium {
 		std::size_t token_vector_index_at_entry = token_vector_index;
 
 		while (is_definition ());
-
-		if (! is_expression ()) {
-			token_vector_index = token_vector_index_at_entry;
-			return false;
-		}
-
-		while (is_expression ());
+		// syntax.txt says that body is zero or more definitions
+		// followed by one or more expressions. This is hard to match.
+		// Hence, allowing zero or more expressions
+		while(is_expression ());
 
 		print_production(token_vector_index_at_entry, "body");
 		return true;
